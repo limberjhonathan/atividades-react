60df45a0d49ccc6c4dbde602f03d87dfcd4fb907
Limber Jhonathan <limberjhonathan@gmail.com>
2024-06-26 22:51:50 -0300

    Adição de icone

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,21 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 export default function Footer(){
     return (
-    <footer>Rodapé</footer>
+    <footer>
+        <ul className={styles.social_list}>
+            <li>
+                <FaFacebook />
+            </li>
+            <li>
+                <FaInstagram />
+            </li>
+            <li>
+                <FaLinkedin />
+            </li>
+        </ul>
+        <p>Meu footer</p>
+    </footer>
     )
 }
